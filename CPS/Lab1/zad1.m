d80ae6527078b1ac5f7ba74426ea865513a0d3b3
Mateusz Bąk <bakmateusz@icloud.com>
2019-03-12 08:20:21 +0100

    Zad 1 - lab 1 done

---
+++ CPS/Lab1/zad1.m
@@ -0,0 +1,38 @@
+function zad1()
+
+% Narysuj na jednym diagramie wykresy trzech przebiegów
+% sinusoidalnych o czestotliwo?sciach 5, 40, 65 Hz. 
+% Probkownie ma czestotliwosc 60 Hz.
+
+clear figure
+
+f1 = 1;
+f2 = 40;
+f3 = 65;
+
+fp = 60;
+n = 0:1:fp-1;
+dts = 1 / fp;
+time = 0:1/fp:1;
+time(end) = [];
+t = 0:0.001:1;
+
+y1 = sin(2*pi*f1*n*dts);
+y2 = sin(2*pi*f2*n*dts);
+y3 = sin(2*pi*f3*n*dts);
+
+yy1 = sin(2*pi*f1*t);
+yy2 = sin(2*pi*f2*t);
+yy3 = sin(2*pi*f3*t);
+
+subplot(3,1,1)
+plot(time,y1,'o--r'); hold on;
+plot(t,yy1,'b');
+
+subplot(3,1,2)
+plot(time,y2,'o--r'); hold on;
+plot(t,yy2,'b');
+
+subplot(3,1,3)
+plot(time,y3,'o--r'); hold on;
+plot(t,yy3,'b');
